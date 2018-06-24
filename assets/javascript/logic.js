43a0ba76231c7b2a161508aefb9983b63e43f058
Rebecca Gemeinhardt <rebecca.geme@gmail.com>
2018-06-24 09:51:22 -0500

    nav bar links

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $(".img-responsive").hover(function () {
-        $(this).css("background-image", "url(./assets/img/MyName2.jpg)");  
+        $(this).css("background-image", "url(./assets/img/MyName190.jpg)");  
     }, function() {
         $(this).css("background-image", "url(./assets/img/RG-Pic.jpg)");
     });
