16584c176f64057bd2fd4fef736a10c648558e7e
Rebecca Gemeinhardt <rebecca.geme@gmail.com>
2018-06-24 06:28:43 -0500

    finished bio page

---
+++ assets/javascript/logic.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".img-responsive").hover(function () {
+        $(this).css("background-image", "url(./assets/img/MyName2.jpg)");  
+    }, function() {
+        $(this).css("background-image", "url(./assets/img/RG-Pic.jpg)");
+    });
+});
+
+
